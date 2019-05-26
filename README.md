d638c2389c0010d787d9c9dffe3a1afa4ae93f4f
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2019-05-26 15:35:53 +0600

    update title, remove react logo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Text Translator app with ReactJS and Redux
+
+Run server and client:
+> npm run dev
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Reader from "./components/Reader.jsx";
 import LangDownload from "./components/LangDownload.jsx";
@@ -12,8 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Learn a language by reading!</h1>
         </header>
         <LangsPanel />
         <Reader text={testText} />
