072902d476bba305287bf3156e3e94af69e6dac4
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2019-05-27 00:25:26 +0600

    add style variables; turn App  into function component; move App styles to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from "react";
-import "./App.css";
-import Reader from "./components/Reader.jsx";
-import LangDownload from "./components/LangDownload.jsx";
-import LangsPanel from "./components/LangsPanel.jsx";
+import React from 'react';
+import { css } from 'emotion';
+import cssValues from './data/cssValues';
+import Reader from './components/Reader';
+import LangDownload from './components/LangDownload';
+import LangsPanel from './components/LangsPanel';
 
-class App extends Component {
-  render() {
-    const testText =
-      "Offiziell sollen die regierungskritischen Proteste im Iran beendet sein - doch sie gehen weiter.";
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Learn a language by reading!</h1>
-        </header>
-        <LangsPanel />
-        <Reader text={testText} />
-        <LangDownload />
-      </div>
-    );
-  }
-}
+const headerStyle = css`
+  background-color: ${cssValues.colors.black};
+  height: 60px;
+  padding: 10px;
+  color: ${cssValues.colors.cyan};
+`;
+
+const App = () => {
+  const testText =
+    'Offiziell sollen die regierungskritischen Proteste im Iran beendet sein - doch sie gehen weiter.';
+  return (
+    <div className="App">
+      <header className={headerStyle}>
+        <h1 className="App-title">Learn a language by reading!</h1>
+      </header>
+      <LangsPanel />
+      <Reader text={testText} />
+      <LangDownload />
+    </div>
+  );
+};
 
 export default App;
---
+++ client/src/data/cssValues.js
@@ -0,0 +1,6 @@
+export default {
+  colors: {
+    black: '#222',
+    cyan: 'cyan',
+  },
+};
